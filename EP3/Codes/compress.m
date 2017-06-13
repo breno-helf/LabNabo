cb68d5782a57cc12d0d359bda97156620e5b4a23
breno-helf <breno.moura@hotmail.com>
2017-06-13 18:26:15 -0300

    Imagens

--- EP3/Codes/compress.m
+++ EP3/Codes/compress.m
@@ -9,34 +9,51 @@ malha dada.
 
 #}
 
-function compress (ImgName, Kx, Ky, Nx, Ny, type)
-  Kx++;
-  Ky++;
+function compress (ImgName, Nx, Ny, type)
   
-  img = imread(ImgName)(1 : Kx : end, 1 : Ky : end, :);
-  imwrite(img, 'pre-interpola.png', 'Quality', 100);
+  img = imread(ImgName);
   img = double(img);
   
   [a, b, c] = size(img)
+
+  printf ("Compressão %f\n", (double((Nx * Ny))) / (double((a * b))));
   
-  M = reshape (1 : (Nx * Ny * 4), Nx, Ny, 4);
+  xEl = linspace(1, a, Nx);
+  yEl = linspace(1, b, Ny);
+  
+  M = reshape(1 : (Nx * Ny * 4 * c), Nx, Ny, 4, c);
+  A = reshape(1 : (Nx * Ny * c), Nx, Ny, c);
 
-  [dfx, dfy, d2fxy] = aproxdf (Nx, Ny, 1, 1, a, b, img);
-  s
-  for k = 1 : 4
+  for k = 1 : c
     for i = 1 : Nx
-      for j = 1 : Ny	
-	if (k == 2)
-	  M(i, j, k) = dfx(i, j);
-
-	elseif (k == 3)
-	  M(i, j, k) = dfy(i, j);
+      for j = 1 : Ny
+	x = xEl(i);
+	y = yEl(j);      
+	A(i, j, k) = img(x, y, k);
+      end
+    end
+  end
+  
+  for d = 1 : c
+    [dfx, dfy, d2fxy] = aproxdf(Nx, Ny, 1, 1, a, b, A(:, :, d));
+    for k = 1 : 4   
+      for i = 1 : Nx
+	for j = 1 : Ny
+	  x = xEl(i);
+	  y = yEl(j);
 	  
-	elseif (k == 4)
-	  M(i, j, k) = d2fxy(i, j);
-
-	else
-	  M(i, j, k) = img(i, j);
+	  if (k == 2)
+	    M(i, j, k, d) = dfx(i, j);
+	    
+	  elseif (k == 3)
+	    M(i, j, k, d) = dfy(i, j);
+	  
+	  elseif (k == 4)
+	    M(i, j, k, d) = d2fxy(i, j);
+	    
+	  else
+	    M(i, j, k, d) = img(x, y, d);
+	  end
 	end
       end
     end
@@ -45,16 +62,23 @@ function compress (ImgName, Kx, Ky, Nx, Ny, type)
   
   compressed = zeros(a, b, c);
   
-  L = constroiv (Nx, Ny, 1, 1, a, b, img, 0);
-  C = constroiv (Nx, Ny, 1, 1, a, b, M, 1);
 
   for k = 1 : c
+    if (type == 0)
+      Q = constroiv (Nx, Ny, 1, 1, a, b, M(:, :, :, k), 0);
+    else
+      Q = constroiv (Nx, Ny, 1, 1, a, b, M(:, :, :, k), 1);
+    end
+      
+
     for i = 1 : a
-  
-  
-  for k = 1 : c
-    for i = 1 : a - 1
-      for j = 1 : b - 1
-	if (type == 1)
-	  
+      for j = 1 : b
+	ret = avaliav (i, j, Q, Nx, Ny, 1, 1, a, b, type);
+	compressed(i, j, k) = ret;
+      end
+    end
+  end
+
+  imwrite(uint8(compressed), 'compressed.png', 'Quality', 100);  
+end	  
   
